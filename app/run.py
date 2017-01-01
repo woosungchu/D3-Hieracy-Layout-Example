068fbb983b8cc6fd27242a6e86074f6a97887242
woosungchu <woosungchu@gmail.com>
2017-01-01 19:46:51 +0900

    refactoring

--- app/run.py
+++ app/run.py
@@ -5,17 +5,21 @@ app = Flask(__name__)
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT,'static/')
 
+URLS  = ['basic']
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', urls=URLS)
 
-@app.route('/<file>')
-def example(file):
-    return render_template(file)
+@app.route('/example.html')
+def example():
+    return render_template('example.html')
 
-@app.route('/layout/<file>')
-def seq(file):
-    return render_template('layout/'+file)
+@app.route('/<url>')
+def practice(url):
+    if url == 'example':
+        return render_template('example.html')
+    return render_template('practice.html', url=url)
 
 if __name__ == '__main__':
     app.run(debug=True)
