25a7dff112c266469a28ff0bb15af0b51cf43c69
woosungchu <woosungchu@gmail.com>
2017-01-11 22:13:56 +0900

    add tree and cluster example

--- README.md
+++ README.md
@@ -8,4 +8,5 @@ Trello - <https://trello.com/b/iAXTjgMd/mindmap>
 - http://d3js.org (D3.js official website)
 - http://d3indepth.com/ (Great D3 v4 tutorial)
 - https://bl.ocks.org/d3noob/76d6fa0dff4af77544da9dd69aef9249 (Tree diagram in v4)
+- https://bl.ocks.org/mbostock/e9ba78a2c1070980d1b530800ce7fa2b (tree and cluster)
 - http://jsfiddle.net/ramnathv/gm9cbcq4/ (trasition)
--- app/run.py
+++ app/run.py
@@ -6,20 +6,21 @@ PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT,'static/')
 
 URLS  = ['basic','selections','datajoins','enterexit','marybartlett','d3noob'
-        ,'hierarchy', 'treemap']
+        ,'hierarchy', 'treemap','treeclustertransition']
 
 @app.route('/')
 def index():
     return render_template('index.html', urls=URLS)
 
-@app.route('/example.html')
-def example():
-    return render_template('example.html')
-
 @app.route('/<url>')
 def practice(url):
-    if url == 'example':
+    if url == 'example.html':
         return render_template('example.html')
+
+    #static
+    elif url.endswith('.csv'):
+        return app.send_static_file(url)
+
     return render_template('practice.html', url=url)
 
 if __name__ == '__main__':
