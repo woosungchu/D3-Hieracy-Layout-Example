648533efd406a548eee52336e18f33aa383be24e
woosungchu <woosungchu@gmail.com>
2017-01-12 21:48:11 +0900

    edit run.py

--- app/run.py
+++ app/run.py
@@ -25,5 +25,5 @@ def practice(url):
 
 if __name__ == '__main__':
     app.debug = True;
-    app.port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
